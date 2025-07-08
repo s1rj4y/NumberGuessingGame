c67bc6961fb1b8ee67c7dacab001649d05e238e5
s1rj4y <sergeipfeffer@web.de>
2025-07-08 12:43:52 +0200

    add project

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // further ideas: classes / difficulty settings / GUI
+
+            Console.WriteLine("Welcome,\nthis little game will test your ability to guess a random number from 1 to 100. Enjoy!\n");
+
+            string exit = "";
+           
+            while (exit != "exit")
+            {
+                Random rnd = new Random();
+                int number = rnd.Next(1, 100);
+
+                Console.ReadKey();
+                Console.Clear();
+                Console.Write("Type 'play' to start or 'exit' to close the game: ");
+                exit = Console.ReadLine().ToLower();
+
+                if (exit == "play")
+                {
+                    int attemptChosen;
+                    bool validAttempt = false;
+                    
+                    do
+                    {
+                        Console.Write("\nPlease choose how many attempts you want to have (max. 20): ");
+                        attemptChosen = int.Parse(Console.ReadLine());
+
+                        if (attemptChosen < 1 || attemptChosen > 20)
+                        {
+                            Console.WriteLine("\nInvalid number of attempts.\n");
+                        }
+                        else
+                        {
+                            validAttempt = true;
+                        }
+                            
+                    } while (!validAttempt);
+
+                    int maxAttempt = 20;
+                    int maxHighscore = 100;
+
+                    int numberInput = 0;
+
+                    float highScore = maxHighscore * ((maxAttempt) / attemptChosen);
+
+                    while (numberInput != number && attemptChosen != 0)
+                    {
+                        Console.Write("\nPlease enter a number (1 to 100): ");
+                        numberInput = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine($"\nYou have {attemptChosen - 1} attempts left.\n");
+                        highScore -= 10;
+
+                        if (numberInput < 0 || numberInput > 100)
+                        {
+                            Console.WriteLine("Invalid number.\n");
+                            attemptChosen--;
+                        }
+                        else if (numberInput < number)
+                        {
+                            Console.WriteLine("The random number is bigger.\n");
+                            attemptChosen--;
+                        }
+                        else if (numberInput > number)
+                        {
+                            Console.WriteLine("The random number is smaller.\n");
+                            attemptChosen--;
+                        }
+                        else if (numberInput == number)
+                        {
+                            Console.Write($"Congratulations, you found the number. Your highscore is: {highScore}\nPlease type in your name: ");
+                            string playerName = Console.ReadLine().ToUpper();
+
+                            ScoreBoard.AddScore(highScore, playerName);
+                            ScoreBoard.ShowScore();
+                        }
+                        if (attemptChosen == 0)
+                        {
+                            Console.WriteLine("You are out of attempts. GAME OVER!");
+                        }
+                    }
+                }
+                else if (exit == "exit")
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+            }
+        }
+    }
+}
---
+++ ScoreBoard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuessingGame
+{
+    internal class ScoreBoard
+    {        
+        public float HighScore { get; }
+        public string PlayerName { get; }
+
+        private static List<ScoreBoard> score = new List<ScoreBoard>();
+
+        public ScoreBoard(float highScore, string playerName)
+        {
+            HighScore = highScore;
+            PlayerName = playerName;
+        }
+
+        public static void AddScore(float highScore, string playerName)
+        {
+            int maxScoreNum = 10;
+
+            if (score.Count < maxScoreNum)
+            {
+                score.Add(new ScoreBoard(highScore, playerName));
+                score.Sort((score1, score2) => score2.HighScore.CompareTo(score1.HighScore));
+            }
+            else if (highScore > score[score.Count - 1].HighScore)
+            {
+                score[score.Count - 1] = new ScoreBoard(highScore, playerName);
+                score.Sort((score1, score2) => score2.HighScore.CompareTo(score1.HighScore));
+            }
+            else
+            {
+                Console.WriteLine("\nNot good enough for the Top 10!");
+            }
+        }
+
+        public static void ShowScore()
+        {
+            Console.WriteLine("\n------- Top 10 -------\n");
+
+            foreach (ScoreBoard entry in score)
+            {
+                Console.WriteLine($"{entry.HighScore} : {entry.PlayerName}");
+            }
+            
+        }
+    }
+}
